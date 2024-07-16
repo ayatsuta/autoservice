cf4eba0be5c64d743ba0ab3809dd5a839c7bd392
Artem Yatsuta <yatsutaproject@gmail.com>
2024-07-16 10:18:24 +0200

    Added ordering by name for class Mechanic in models.py

--- workshop/models.py
+++ workshop/models.py
@@ -50,6 +50,9 @@ class Mechanic(models.Model):
     name = models.CharField(max_length=63)
     speciality = models.CharField(max_length=63)
 
+    class Meta:
+        ordering = ("name",)
+
     def __str__(self):
         return f"{self.name} - {self.speciality}"
 
