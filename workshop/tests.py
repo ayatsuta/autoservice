47e78aba305172ae0b6508aa92d577ef391bbea0
Artem Yatsuta <yatsutaproject@gmail.com>
2024-07-25 15:32:45 +0200

    Created tests for Vehicle model

--- workshop/tests.py
+++ workshop/tests.py
@@ -24,33 +24,47 @@ class ManagerModelTests(TestCase):
         )
 
     def test_manager_str(self):
-        manager = get_user_model().objects.create(
-            username="test",
-            password="test1234",
-            first_name="test_first",
-            last_name="test_last",
-        )
         self.assertEqual(
-            str(manager),
-            f"{manager.username} : {manager.first_name} {manager.last_name}"
+            str(self.manager),
+            f"{self.manager.username} : {self.manager.first_name} {self.manager.last_name}"
         )
 
 
 class VehicleModelTests(TestCase):
-    def test_vehicle_str(self):
-        owner = Client.objects.create(name="test_client")
-        vehicle = Vehicle.objects.create(
-            model="test_vehicle",
-            year="2000",
-            owner=owner
+    def setUp(self):
+        self.client = Client.objects.create(name="Client1", phone="123456789")
+        self.manager = get_user_model().objects.create_user(username="manager1", password="password")
+        self.vehicle = Vehicle.objects.create(
+            model="textX",
+            year=2010,
+            VIN="123ABC",
+            owner=self.client,
+            manager=self.manager,
         )
+        self.mechanic = Mechanic.objects.create(name="Mechanic1", speciality="General Repair")
+        self.vehicle.mechanics.add(self.mechanic)
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.vehicle.get_absolute_url(),
+                         reverse("workshop:vehicle-detail", args=[str(self.vehicle.id)]))
+
+    def test_vehicle_mechanics(self):
+        self.assertIn(self.mechanic, self.vehicle.mechanics.all())
+
+    def test_vehicle_manager(self):
+        self.assertEqual(self.vehicle.manager, self.manager)
+
+    def test_vehicle_str(self):
         self.assertEqual(
-            str(vehicle),
-            f"{vehicle.model} - {vehicle.owner}"
+            str(self.vehicle),
+            f"{self.vehicle.model} - {self.vehicle.owner}"
         )
 
 
 class MechanicModelTests(TestCase):
+    def setUp(self):
+        self.mechanic = Mechanic.objects.create(name="Mechanic1", speciality="General Repair")
+
     def test_mechanic_str(self):
         mechanic = Mechanic.objects.create(
             name="test_mechanic",
