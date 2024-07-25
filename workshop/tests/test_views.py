757447db753621df8e62d9e3846324441ff1bc22
Artem Yatsuta <yatsutaproject@gmail.com>
2024-07-25 22:16:17 +0200

    Added tests for toggle assign view & move setup to base test case for views

--- workshop/tests/test_views.py
+++ workshop/tests/test_views.py
@@ -7,6 +7,19 @@ from workshop.models import Mechanic, Vehicle, Client
 User = get_user_model()
 
 
+class BaseViewTests(TestCase):
+    def setUp(self):
+        self.manager_user = User.objects.create_user(
+            username="manager1", password="password"
+        )
+        self.client.login(username="manager1", password="password")
+        self.client_instance = Client.objects.create(name="Client1", phone="123456789")
+        self.vehicle = Vehicle.objects.create(
+            model="TestX", year=2020, VIN="123XYZ", owner=self.client_instance
+        )
+        self.mechanic = Mechanic.objects.create(name="Mechanic1", speciality="Engine Repair")
+
+
 class ManagerViewTests(TestCase):
     def setUp(self):
         self.manager_user = User.objects.create_user(
@@ -30,13 +43,7 @@ class ManagerViewTests(TestCase):
         self.assertTemplateUsed(response, "workshop/manager_detail.html")
 
 
-class MechanicViewTests(TestCase):
-    def setUp(self):
-        self.mechanic_user = User.objects.create_user(
-            username="mechanic1", password="password"
-        )
-        self.client.force_login(self.mechanic_user)
-        self.mechanic = Mechanic.objects.create(name="Mechanic1", speciality="Engine Repair")
+class MechanicViewTests(BaseViewTests):
 
     def test_mechanic_list_view(self):
         response = self.client.get(reverse("workshop:mechanic-list"))
@@ -54,16 +61,7 @@ class MechanicViewTests(TestCase):
         self.assertTemplateUsed(response, "workshop/mechanic_detail.html")
 
 
-class VehicleViewTests(TestCase):
-    def setUp(self):
-        self.client_user = User.objects.create_user(
-            username="client1", password="password"
-        )
-        self.client.login(username="client1", password="password")
-        self.client_instance = Client.objects.create(name="Client1", phone="123456789")
-        self.vehicle = Vehicle.objects.create(
-            model="TestX", year=2000, VIN="123XYZ", owner=self.client_instance
-        )
+class VehicleViewTests(BaseViewTests):
 
     def test_vehicle_list_view(self):
         response = self.client.get(reverse("workshop:vehicle-list"))
@@ -79,3 +77,19 @@ class VehicleViewTests(TestCase):
         response = self.client.get(reverse("workshop:vehicle-detail", args=[self.vehicle.id]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "workshop/vehicle_detail.html")
+
+
+class ToggleAssignManagerToVehicleTests(BaseViewTests):
+    def test_toggle_assign_manager_to_vehicle_assign(self):
+        response = self.client.post(reverse("workshop:toggle-assign-manager", args=[self.vehicle.pk]))
+        self.vehicle.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.vehicle.manager, self.manager_user)
+
+    def test_toggle_assign_manager_to_vehicle_unassign(self):
+        self.vehicle.manager = self.manager_user
+        self.vehicle.save()
+        response = self.client.post(reverse("workshop:toggle-assign-manager", args=[self.vehicle.pk]))
+        self.vehicle.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertIsNone(self.vehicle.manager)
